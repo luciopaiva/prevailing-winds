ff8ce1f15652602fa181e6b2fb073e7cb506619e
Lucio Paiva <luciopaiva@gmail.com>
2019-03-10 22:40:33 -0300

    Wind cells, textures

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const PLANET_RESOLUTION = 64;
+const PLANET_RADIUS = 0.5;
 
 class App {
 
@@ -16,6 +17,8 @@ class App {
         document.querySelector(".container").appendChild(this.renderer.domElement);
 
         this.planet = this.createPlanet();
+        this.loadPlanetTextures();
+        this.winds = this.createWindsNonRotatingPlanet();
 
         this.camera.position.set(0, 0, 2.5);
         this.camera.lookAt(this.planet.position);
@@ -23,6 +26,7 @@ class App {
         this.scene.add(this.camera);
         this.scene.add(this.spotlight);
         this.scene.add(this.planet);
+        this.scene.add(this.winds);
 
         window.addEventListener("resize", this.onResize.bind(this));
         requestAnimationFrame(this.render.bind(this));
@@ -39,16 +43,78 @@ class App {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
+    /**
+     * Draws wind patterns in a planet that is not rotating (so there's no Coriolis force).
+     *
+     * @return {THREE.Object3D}
+     */
+    createWindsNonRotatingPlanet() {
+        const TAU = Math.PI * 2;
+        const HALF_PI = Math.PI / 2;
+
+        const altitude1 = PLANET_RADIUS * 1.05;
+        const altitude2 = PLANET_RADIUS * 1.2;
+
+        const winds = new THREE.Object3D();
+
+        const lonAngleStep = TAU / 12;
+        const zoneStep = Math.PI / 6;  // 6 zones
+
+        for (let lon = 0; lon < TAU; lon += lonAngleStep) {
+            for (let zone = 0; zone < Math.PI; zone += zoneStep) {
+                const latStart = zone + zoneStep * 0.05;
+                const latEnd = zone + zoneStep - zoneStep * 0.05;
+                const latStep = (latEnd - latStart) / 3;
+
+                const curve = new THREE.CatmullRomCurve3([
+                    (new THREE.Vector3()).setFromSphericalCoords(altitude1, latStart, lon),
+                    (new THREE.Vector3()).setFromSphericalCoords(altitude1, latStart + latStep, lon),
+                    (new THREE.Vector3()).setFromSphericalCoords(altitude1, latStart + latStep * 2, lon),
+                    (new THREE.Vector3()).setFromSphericalCoords(altitude1, latEnd, lon),
+                    (new THREE.Vector3()).setFromSphericalCoords(altitude2, latEnd, lon),
+                    (new THREE.Vector3()).setFromSphericalCoords(altitude2, latEnd - latStep, lon),
+                    (new THREE.Vector3()).setFromSphericalCoords(altitude2, latEnd - latStep * 2, lon),
+                    (new THREE.Vector3()).setFromSphericalCoords(altitude2, latStart, lon),
+                ], true);
+
+                const points = curve.getPoints(800);
+                const geometry = (new THREE.BufferGeometry()).setFromPoints(points);
+                const material = new THREE.LineBasicMaterial({ color: 0x444444, linewidth: 2 });
+
+                winds.add(new THREE.Line(geometry, material));
+            }
+        }
+
+        return winds;
+    }
+
+    loadPlanetTextures() {
+        const mapLoader = new THREE.TextureLoader();
+        mapLoader.load("textures/map.jpg", texture => {
+            this.planet.material.map = texture;
+            this.planet.material.needsUpdate = true;
+        });
+
+        const bumpMapLoader = new THREE.TextureLoader();
+        bumpMapLoader.load("textures/bump.jpg", texture => {
+            this.planet.material.bumpMap = texture;
+            this.planet.material.needsUpdate = true;
+        });
+
+        // map: https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthmap1k.jpg
+        // bump map: https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthbump1k.jpg
+        // specular map: https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthspec1k.jpg
+    }
+
     createPlanet() {
-        const size = 0.5;
+        const size = PLANET_RADIUS;
         const geometry = new THREE.SphereGeometry(size, PLANET_RESOLUTION, PLANET_RESOLUTION);
         const material = new THREE.MeshPhongMaterial({
             bumpScale: 0.05,
             specular: new THREE.Color("black"),
             shininess: 0,
         });
-        const surface = new THREE.Mesh(geometry, material);
-        return surface;
+        return new THREE.Mesh(geometry, material);
     }
 }
 
